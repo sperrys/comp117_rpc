7aa32352acb89642afad6b785d9cf445ebef62ac
Aaron Bowen <aaron.bwn@gmail.com>
2017-04-12 17:53:35 -0400

    Add namespace and include string to (de)serialization implementations

--- rpc_utility_funcs.py
+++ rpc_utility_funcs.py
@@ -84,7 +84,8 @@ def generate_serial_header(idl_types, filename):
 # generates the corresponding 
 # serialization implementation content and returns it
 def generate_serial_imp(idl_types):
-	imp_content = "//                  serilization.cpp\n//\n//     This is generated implementation file for the serialization \n\n\n#include \"generic_serialization.h\"\n#include \"gen.serialization.h\"\n\n\n"
+	imp_content = "//                  serilization.cpp\n//\n//     This is generated implementation file for the serialization \n\n\n"
+	imp_content += "using namespace std;\n#include <string>\n\n#include \"generic_serialization.h\"\n#include \"gen.serialization.h\"\n\n\n"
 	for name, sig in idl_types:
 		imp_content += serializer.construct_decl(name, sig, False) + serializer.construct_body(name, sig)
 	return imp_content
@@ -104,7 +105,8 @@ def generate_deserial_header(idl_types, filename):
 # generates the corresponding 
 # deserialization implementation content and returns it
 def generate_deserial_imp(idl_types):
-	imp_content = "//                  deserilization.cpp\n//\n//     This is generated header file for the deserialization \n\n\n#include \"generic_deserialization.h\"\n#include \"gen.deserialization.h\"\n\n\n"
+	imp_content = "//                  deserilization.cpp\n//\n//     This is generated implementation file for the deserialization \n\n\n"
+	imp_content += "using namespace std;\n#include <string>\n\n#include \"generic_deserialization.h\"\n#include \"gen.deserialization.h\"\n\n\n"
 	for name, sig in idl_types:
 		imp_content += deserializer.construct_decl(name, sig, False) + deserializer.construct_body(name, sig)
 	return imp_content
