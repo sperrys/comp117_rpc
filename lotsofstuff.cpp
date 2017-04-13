386675311f65021bb079f717ffe68d88827cce47
Spencer F. Perry <sperry02@vm-hw05.eecs.tufts.edu>
2017-04-13 17:22:31 -0400

    fixed errors in lotsofstuff client

--- lotsofstuff.cpp
+++ lotsofstuff.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 #include "lotsofstuff.idl"
 #include <stdexcept>
+#include <iostream>
 
 void person_func(Person p) {
 
@@ -17,13 +18,13 @@ void person_func(Person p) {
  }
 
  void people_func(ThreePeople people) {
- 	cout << "person1 name" << people->-p1->firstname;
- 	cout << "person2 name" << people->-p2->firstname;
- 	cout << "person3 name" << people->-p3->firstname;
+ 	cout << "person1 name" << people.p1.firstname;
+ 	cout << "person2 name" << people.p2.firstname;
+ 	cout << "person3 name" << people.p3.firstname;
  }
 
  int sum(int x[3]) {
- 	sum = 0; 
+ 	int sum = 0; 
  	for (int i = 0; i < 3; i++) {
  		sum += i; 
  	}
